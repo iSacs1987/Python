bc87a101bd85164780ae7f8d5f436dd2e545ad2b
iSacs1987 <christoph-knorr@gmx.de>
2022-05-16 08:02:49 +0200

    better variable names

--- Printing.py
+++ Printing.py
@@ -1,6 +1,6 @@
-Var1 = "Hello"
-Var2 = "World"
-Var3 = "Christoph Knorr"
-print(Var3)
-print("My name is",Var3)
-print(f"Learning programming starts with {Var1} {Var2}")
+test_var1 = "Hello"
+test_var2 = "World"
+test_var3 = "Christoph Knorr"
+print(test_var3)
+print("My name is",test_var3)
+print(f"Learning programming starts with {test_var1} {test_var2}")
